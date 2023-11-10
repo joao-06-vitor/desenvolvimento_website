5a897a673bfbdf99e70cbc89a1643948f462472b
EA - João Vitor Ramalho Lopes <vitor_ramalho@educadventista.org>
2023-11-10 09:50:26 -0300

    3 commit

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const app = express ()
 
 const caminhoBase = path.join(__dirname,"templates") 
 
-app.get ('/', (requisicao, resposta) => {
-    resposta.sendFile(`${caminhoBase}/index.html´)
+app.get('/', (requisicao, resposta) => {
+    resposta.sendFile(`${caminhoBase}/index.html`)
+ 
 })
 
-app. Listen (3000, () => {
+app.Listen (3000, () => {
     console. log ("Servidor rodando na porta 3000!")
 })
